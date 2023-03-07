ea8345cccc652ba33c3857bbf4f51d8c3b72bd51
John San Juan <98930957+mrsamsonn@users.noreply.github.com>
2023-03-06 22:46:57 -0800

    updated email

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        'service_5rozlso',
+        'template_kab0mvk',
         {
           from_name: form.name,
           to_name: "John",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "andrewsj0006@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        'FzzxgjmdsIFRPbV_V',
       )
       .then(
         () => {
