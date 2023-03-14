e6634fd344dbada92e777d6746936591091d361d
John <98930957+mrsamsonn@users.noreply.github.com>
2023-03-13 22:04:50 -0700

    update commit command

--- push.sh
+++ push.sh
@@ -4,13 +4,13 @@ git push origin --delete gh-pages
 echo "Commit Message: "
 read commit
 
-git commit -m "'"$commit"'"
+git commit -m "$commit"
 git push
 
 npm run build
 git add dist -f
 
-git commit -m "'"$commit"'"
+git commit -m "$commit"
 
 git subtree push --prefix dist origin gh-pages
 git push
